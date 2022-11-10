51e99392a1c8acf3bd544cd7a65b585858fc0fd4
Maximilian Geiger <maximilian.geiger2@gmail.com>
2022-11-10 16:29:50 +0100

    add something idk havent comittet in a while

--- testapplication/lib/Navigation.dart
+++ testapplication/lib/Navigation.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:testapplication/favoritesList/FavoritesList.dart';
 import 'package:testapplication/learningList/LearningList.dart';
 import 'package:testapplication/vocabulary/Word.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Navigation extends StatelessWidget {
   @override
@@ -29,9 +30,30 @@ class Navigation extends StatelessWidget {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => FavoriteList()));
             },
+          ),
+          AboutListTile(
+            icon: Icon(Icons.info),
+            child: const Text("About"),
+            applicationIcon: Icon(Icons.local_play),
+            applicationName: "Language Helper",
+            applicationVersion: "0.1.0",
+            applicationLegalese: "© Maximilian Geiger",
+            aboutBoxChildren: [
+              InkWell(
+                onTap: _launchURL,
+                child: const Text("Icon: designed by Iconixar from Flaticon"),
+              )
+            ],
           )
         ],
       ),
     );
+  }
+
+  _launchURL() async {
+    const url = "https://www.flaticon.com/de/autoren/iconixar";
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
   }
 }
